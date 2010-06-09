89b4353539f21fd4138ea6dd8f62cb0d65fc8614
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2010-06-08 18:21:09 -0700

    Scope by fields for connections.

--- lib/fogli/facebook_object/scope_methods.rb
+++ lib/fogli/facebook_object/scope_methods.rb
@@ -7,13 +7,41 @@ module Fogli
       # associated with it, which is used to build up a new scope.
       [:limit, :offset, :until, :since].each do |scope|
         define_method(scope) do |value|
-          args = if self.is_a?(ConnectionScope)
-            [proxy, options.merge(scope => value)]
-          else
-            [self, {scope => value}]
-          end
+          ConnectionScope.new(*_scope_initializer_args(scope, value))
+        end
+      end
 
-          ConnectionScope.new(*args)
+      # Scope the connection by the fields you're interested in. By
+      # default, the connection will load all the possible fields for
+      # the connection, which can result in a fairly large overhead if
+      # you're only interested in the `first_name`, for example.
+      #
+      # The parameter to this method can be almost anything which can
+      # intuitively be converted down to either a string or
+      # array. Some example uses follow (admittingly some of these are
+      # a bit pathological, but they're meant to showcase the
+      # flexibility of the method):
+      #
+      #     fields("name,birthday,last_name")
+      #     fields(:name, :birthday, :last_name)
+      #     fields([:name, "birthday"], :last_name)
+      #     fields("name,birthday", ["last_name"])
+      #
+      def fields(*fields)
+        ConnectionScope.new(*_scope_initializer_args(:fields, fields.flatten.join(",")))
+      end
+
+      # Returns the proper set of arguments to send to the
+      # {ConnectionScope} initializer given the scope and value.
+      #
+      # @param [Symbol] scope The scope option.
+      # @param [Object] value The value for the scope.
+      # @return [Array]
+      def _scope_initializer_args(scope, value)
+        if self.is_a?(ConnectionScope)
+          [proxy, options.merge(scope => value)]
+        else
+          [self, {scope => value}]
         end
       end
     end
--- test/fogli/facebook_object/scope_methods_test.rb
+++ test/fogli/facebook_object/scope_methods_test.rb
@@ -5,7 +5,7 @@ class ScopeMethodsTest < Test::Unit::TestCase
     @module = Fogli::FacebookObject::ScopeMethods
   end
 
-  context "not in a connection scope" do
+  context "scope methods" do
     setup do
       @klass = Class.new
       @klass.send(:include, @module)
@@ -20,22 +20,49 @@ class ScopeMethodsTest < Test::Unit::TestCase
         assert_equal @instance, result.proxy
       end
     end
+
+    should "initialize a scope for fields" do
+      tests = [
+               ["name,birthday,location"],
+               [:name, :birthday, :location],
+               [["name", :birthday], :location],
+               [["name"], [[[:birthday]], :location]]
+              ]
+
+      tests.each do |args|
+        scope = @instance.fields(*args)
+        assert_equal "name,birthday,location", scope.options[:fields]
+      end
+    end
   end
 
-  context "in a connection scope" do
-    setup do
-      @proxy = mock("proxy")
-      @options = { :foo => :bar }
-      @instance = Fogli::FacebookObject::ConnectionScope.new(@proxy, @options)
+
+  context "scope initializer args" do
+    context "not a connection scope" do
+      setup do
+        @klass = Class.new
+        @klass.send(:include, @module)
+        @instance = @klass.new
+      end
+
+      should "return the proxy if not a connection scope" do
+        proxy, options = @instance._scope_initializer_args(:foo, :bar)
+        assert_equal @instance, proxy
+        assert_equal({:foo => :bar}, options)
+      end
     end
 
-    [:limit, :offset, :until, :since].each do |scope|
-      should "return a new connection scope for #{scope}, with options merged" do
-        value = "#{scope}_foo"
-        result = @instance.send(scope, value)
-        assert_equal value, result.options[scope]
-        assert_equal :bar, result.options[:foo]
-        assert_equal @proxy, result.proxy
+    context "in a connection scope" do
+      setup do
+        @proxy = mock("proxy")
+        @options = { :foo => :bar }
+        @instance = Fogli::FacebookObject::ConnectionScope.new(@proxy, @options)
+      end
+
+      should "return the proxy and merged options" do
+        proxy, options = @instance._scope_initializer_args(:bar, :baz)
+        assert_equal @proxy, proxy
+        assert_equal(@options.merge(:bar => :baz), options)
       end
     end
   end
